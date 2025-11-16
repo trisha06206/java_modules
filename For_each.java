1c768801fe68311790264846f704516302f49e27
trisha06206 <trishatrisham09@gmail.com>
2025-11-16 13:39:00 +0530

    Create For_each.java

---
+++ For_each.java
@@ -0,0 +1,11 @@
+immport java.util.*;
+
+public class For_each{
+  public static void main(string[]args {
+      Scanner sc=new Scanner (System.in);
+    String[] fruits = {"Apple","Banana","Cherry"};
+    for(String fruit:fruit){
+      System.out.println("Fruit: "+fruit);
+    }
+  }
+}
