d8c87c3eaa0a3909656aae157e35fd0c36ddf4c0
trisha06206 <trishatrisham09@gmail.com>
2025-11-16 13:24:08 +0530

    Create Day.java

---
+++ Day.java
@@ -0,0 +1,33 @@
+import java.util.*;
+public class Dat{
+  public static void main(string[]args){
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter the number of the day(1-7): ");
+    int day = sc.nextInt();
+
+    switch(day){
+      case 1:
+        System.out.println("Monday");
+        break;
+      case 2:
+        System.out.println("Tuesday");
+        break;
+      case 3:
+        System.out.println("Wednesday");
+        break;
+      case 4:
+        System.out.println("Thursday");
+        break;
+      case 5:
+        System.out.println("Friday");
+        break;
+      case 6:
+        System.out.println("saturday");
+        break;
+      case 7:
+        System.out.println("Sunday");
+        break;
+      default:
+        System.out.println("Invalid value!");
+    }
+  }
