75e3dac2f1856ce293b1b8d0919613afebe4d591
trisha06206 <trishatrisham09@gmail.com>
2025-11-16 09:42:20 +0530

    Update and rename README.md to Array_basic.java

---
+++ Array_basic.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+class Array_basic{
+  public static void main(String[]args){
+    int[] arr={1,4,9,16,25,36,49,64,81,100};
+
+  System.out.println("The valueon 4 Position is :"+ arr[3]);
+  }
+}
