4582e8a51452414a240b5ad200a5eef808cb4abd
trisha06206 <trishatrisham09@gmail.com>
2025-11-16 09:21:59 +0530

    Create array_1.java

---
+++ array_1.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Array_1{
+  public static void main(string[] args){
+    scanner sc=new scanner(system.in);
+    int n []=new int[5];
+    System.out.println("enter five number:");
+    for(int i=0;i<5;i++){
+      n[i]=sc.nextInt();
+    }
+    System.out.println("Our Array has following numbers:");
+    for(int i=0;i<5;i++){
+      system.out.println(n[i]);
+    }
+  }
+}
