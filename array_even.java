02c8901d54d2afcb313224f36e3ef3e633bc1106
trisha06206 <trishatrisham09@gmail.com>
2025-11-16 09:48:37 +0530

    Create array_even.java

---
+++ array_even.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Array_event{
+  public static void main(string[]args){
+    int arr[]={23,45,67,89,23,43,12,45,87,34,90,43};
+    System.out.println("Even numbers present in the array are: ");
+    for (int i=0;i<12;i++){
+      if(arr[i]%2==0){
+        System.out,println(arr[i]);
+      }
+    }
+  }
+}
