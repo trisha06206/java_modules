31c3f6629956317de622628272e9a210a39738b3
trisha06206 <trishatrisham09@gmail.com>
2025-11-16 10:28:38 +0530

    Create ConditionalOperation.java

---
+++ ConditionalOperation.java
@@ -0,0 +1,7 @@
+public class ConditionalOperation{
+  public static void main(string[]args){
+    int a =98;
+    String result = (a%3==0)?"Divide by 3":"Not divide by 3";
+    System.out.println("The number "+a" is "+result);
+  }
+}
