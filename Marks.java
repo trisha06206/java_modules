d027539ab13cbc7ac4b8537fdb519421434702ac
trisha06206 <trishatrisham09@gmail.com>
2025-11-18 13:52:41 +0530

    Create Marks.java

---
+++ Marks.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Marks {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter Marks of Three subject (0-100)");
+        float a=sc.nextFloat();
+        float b=sc.nextFloat();
+        float c=sc.nextFloat();
+        float d=a+b+c;
+        float e=d/3;
+        System.out.println("Total Marks :"+d);
+        System.out.println("Average Marks :"+e);
+        sc.close();    
+    }
+}
