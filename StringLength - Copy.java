4c69c6a30ef7cde5c4f32d1c0f4fb66f7b3cb7ff
trisha06206 <trishatrisham09@gmail.com>
2025-11-18 14:53:59 +0530

    Create StringLength - Copy.java

---
+++ StringLength - Copy.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class StringLength {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a string: ");
+        String s1=sc.nextLine();
+        System.out.println("Length of the string :"+s1.length());
+        sc.close();
+    }    
+}
