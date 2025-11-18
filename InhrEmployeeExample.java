757798217f277f63519f4e19f89894eb772fb209
trisha06206 <trishatrisham09@gmail.com>
2025-11-18 13:35:15 +0530

    Create InhrEmployeeExample.java

---
+++ InhrEmployeeExample.java
@@ -0,0 +1,316 @@
+class InhrEmployee {
+    String name;
+    double salary;
+
+    void setData(String name, double salary) {
+        this.name = name;
+        this.salary = salary;
+    }
+
+    void displayInfo() {
+        System.out.println("Name: " + name);
+        System.out.println("Salary: ₹" + salary);
+    }
+}
+
+class InhrManager extends InhrEmployee {
+    String department;
+
+    void setDepartment(String department) {
+        this.department = department;
+    }
+
+    void displayInfo() {
+        super.displayInfo();
+        System.out.println("Department: " + department);
+    }
+}
+
+public class InhrEmployeeExample {
+    public static void main(String[] args) {
+        InhrManager m1 = new InhrManager();
+        m1.setData("Phoenix", 85000);
+        m1.setDepartment("Software Development");
+        m1.displayInfo();
+    }
+}class InhrEmployee {
+    String name;
+    double salary;
+
+    void setData(String name, double salary) {
+        this.name = name;
+        this.salary = salary;
+    }
+
+    void displayInfo() {
+        System.out.println("Name: " + name);
+        System.out.println("Salary: ₹" + salary);
+    }
+}
+
+class InhrManager extends InhrEmployee {
+    String department;
+
+    void setDepartment(String department) {
+        this.department = department;
+    }
+
+    void displayInfo() {
+        super.displayInfo();
+        System.out.println("Department: " + department);
+    }
+}
+
+public class InhrEmployeeExample {
+    public static void main(String[] args) {
+        InhrManager m1 = new InhrManager();
+        m1.setData("Phoenix", 85000);
+        m1.setDepartment("Software Development");
+        m1.displayInfo();
+    }
+}class InhrEmployee {
+    String name;
+    double salary;
+
+    void setData(String name, double salary) {
+        this.name = name;
+        this.salary = salary;
+    }
+
+    void displayInfo() {
+        System.out.println("Name: " + name);
+        System.out.println("Salary: ₹" + salary);
+    }
+}
+
+class InhrManager extends InhrEmployee {
+    String department;
+
+    void setDepartment(String department) {
+        this.department = department;
+    }
+
+    void displayInfo() {
+        super.displayInfo();
+        System.out.println("Department: " + department);
+    }
+}
+
+public class InhrEmployeeExample {
+    public static void main(String[] args) {
+        InhrManager m1 = new InhrManager();
+        m1.setData("Phoenix", 85000);
+        m1.setDepartment("Software Development");
+        m1.displayInfo();
+    }
+}class InhrEmployee {
+    String name;
+    double salary;
+
+    void setData(String name, double salary) {
+        this.name = name;
+        this.salary = salary;
+    }
+
+    void displayInfo() {
+        System.out.println("Name: " + name);
+        System.out.println("Salary: ₹" + salary);
+    }
+}
+
+class InhrManager extends InhrEmployee {
+    String department;
+
+    void setDepartment(String department) {
+        this.department = department;
+    }
+
+    void displayInfo() {
+        super.displayInfo();
+        System.out.println("Department: " + department);
+    }
+}
+
+public class InhrEmployeeExample {
+    public static void main(String[] args) {
+        InhrManager m1 = new InhrManager();
+        m1.setData("Phoenix", 85000);
+        m1.setDepartment("Software Development");
+        m1.displayInfo();
+    }
+}class InhrEmployee {
+    String name;
+    double salary;
+
+    void setData(String name, double salary) {
+        this.name = name;
+        this.salary = salary;
+    }
+
+    void displayInfo() {
+        System.out.println("Name: " + name);
+        System.out.println("Salary: ₹" + salary);
+    }
+}
+
+class InhrManager extends InhrEmployee {
+    String department;
+
+    void setDepartment(String department) {
+        this.department = department;
+    }
+
+    void displayInfo() {
+        super.displayInfo();
+        System.out.println("Department: " + department);
+    }
+}
+
+public class InhrEmployeeExample {
+    public static void main(String[] args) {
+        InhrManager m1 = new InhrManager();
+        m1.setData("Phoenix", 85000);
+        m1.setDepartment("Software Development");
+        m1.displayInfo();
+    }
+}class InhrEmployee {
+    String name;
+    double salary;
+
+    void setData(String name, double salary) {
+        this.name = name;
+        this.salary = salary;
+    }
+
+    void displayInfo() {
+        System.out.println("Name: " + name);
+        System.out.println("Salary: ₹" + salary);
+    }
+}
+
+class InhrManager extends InhrEmployee {
+    String department;
+
+    void setDepartment(String department) {
+        this.department = department;
+    }
+
+    void displayInfo() {
+        super.displayInfo();
+        System.out.println("Department: " + department);
+    }
+}
+
+public class InhrEmployeeExample {
+    public static void main(String[] args) {
+        InhrManager m1 = new InhrManager();
+        m1.setData("Phoenix", 85000);
+        m1.setDepartment("Software Development");
+        m1.displayInfo();
+    }
+}class InhrEmployee {
+    String name;
+    double salary;
+
+    void setData(String name, double salary) {
+        this.name = name;
+        this.salary = salary;
+    }
+
+    void displayInfo() {
+        System.out.println("Name: " + name);
+        System.out.println("Salary: ₹" + salary);
+    }
+}
+
+class InhrManager extends InhrEmployee {
+    String department;
+
+    void setDepartment(String department) {
+        this.department = department;
+    }
+
+    void displayInfo() {
+        super.displayInfo();
+        System.out.println("Department: " + department);
+    }
+}
+
+public class InhrEmployeeExample {
+    public static void main(String[] args) {
+        InhrManager m1 = new InhrManager();
+        m1.setData("Phoenix", 85000);
+        m1.setDepartment("Software Development");
+        m1.displayInfo();
+    }
+}class InhrEmployee {
+    String name;
+    double salary;
+
+    void setData(String name, double salary) {
+        this.name = name;
+        this.salary = salary;
+    }
+
+    void displayInfo() {
+        System.out.println("Name: " + name);
+        System.out.println("Salary: ₹" + salary);
+    }
+}
+
+class InhrManager extends InhrEmployee {
+    String department;
+
+    void setDepartment(String department) {
+        this.department = department;
+    }
+
+    void displayInfo() {
+        super.displayInfo();
+        System.out.println("Department: " + department);
+    }
+}
+
+public class InhrEmployeeExample {
+    public static void main(String[] args) {
+        InhrManager m1 = new InhrManager();
+        m1.setData("Phoenix", 85000);
+        m1.setDepartment("Software Development");
+        m1.displayInfo();
+    }
+}class InhrEmployee {
+    String name;
+    double salary;
+
+    void setData(String name, double salary) {
+        this.name = name;
+        this.salary = salary;
+    }
+
+    void displayInfo() {
+        System.out.println("Name: " + name);
+        System.out.println("Salary: ₹" + salary);
+    }
+}
+
+class InhrManager extends InhrEmployee {
+    String department;
+
+    void setDepartment(String department) {
+        this.department = department;
+    }
+
+    void displayInfo() {
+        super.displayInfo();
+        System.out.println("Department: " + department);
+    }
+}
+
+public class InhrEmployeeExample {
+    public static void main(String[] args) {
+        InhrManager m1 = new InhrManager();
+        m1.setData("Phoenix", 85000);
+        m1.setDepartment("Software Development");
+        m1.displayInfo();
+    }
+}
