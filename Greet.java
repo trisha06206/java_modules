04144c790117492a7093286f3bf228f65b344eae
trisha06206 <trishatrisham09@gmail.com>
2025-11-18 12:43:16 +0530

    Create Greet.java

---
+++ Greet.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Greet{
+  public static void main(string[]args){
+    Scanner sc=new Scanner(System.in);
+    System.out.println("Enter your name:\t");
+    String name=sc.nextLine();
+    System.out.println("Enter your age:\t");
+    int age=sc.nextInt();
+
+  System.out.println("Hello "+name+"!Your age is "+age+".");
+  }
+
+}
