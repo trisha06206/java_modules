8153427b702a1f93f4952ecdec74cd4ad3dd8a8c
trisha06206 <trishatrisham09@gmail.com>
2025-11-18 15:08:07 +0530

    Create Square.java

---
+++ Square.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class square{
+    public static void main(String []args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter any number:\t");
+        int n= sc.nextInt();
+        int s=n*n;
+
+        System.out.println("The square of the number is: "+s);
+
+    }
+}
