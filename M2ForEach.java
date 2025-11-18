e5aa6b43a684dbd8f54d0bd9dc0fc2d70d4a4f87
trisha06206 <trishatrisham09@gmail.com>
2025-11-18 13:44:39 +0530

    Create M2ForEach.java

---
+++ M2ForEach.java
@@ -0,0 +1,9 @@
+public class M2ForEach {
+    public static void main(String[] args) {
+        String[] fruits = {"Apple", "Banana", "Mango", "Orange"};
+
+        for (String fruit : fruits) {
+            System.out.println(fruit);
+        }
+    }
+}
