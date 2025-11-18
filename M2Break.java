204e1e6dfd821718756d26693e96a8e36ed7ecdf
trisha06206 <trishatrisham09@gmail.com>
2025-11-18 13:27:32 +0530

    Create M2Break.java

---
+++ M2Break.java
@@ -0,0 +1,11 @@
+public class M2Break {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            if (i == 5) {
+                System.out.println("Breaking at i = " + i);
+                break;
+            }
+            System.out.println("i = " + i);
+        }
+    }
+}
